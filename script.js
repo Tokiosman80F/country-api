bfbdc1601a050b5443667e6d9440fc9dc54b41b7
Tokiosman80F <tokiosman96@gmail.com>
2026-03-31 07:23:45 +0600

    pratracing ajax

--- script.js
+++ script.js
@@ -10,3 +10,48 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+
+const render = function (data) {
+  const languages = Object.values(data.languages).join(',');
+  const currencyName = Object.values(data.currencies)[0].name;
+  const html = `<article class="country">
+          <img class="country__img" src="${data.flags.png}" alt='${data.flags.alt}' />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.official}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${languages} </p>
+            <p class="country__row"><span>💰</span>${currencyName} </p>
+          </div>
+        </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryData = function (name) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${name}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    render(data);
+
+    // get neighbour country
+    const [neighbour] = data.borders;
+    // if no neighbout found then return
+    if (!neighbour) return;
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log('Data 2', data2);
+      render(data2);
+    });
+  });
+};
+
+getCountryData('bangladesh');
